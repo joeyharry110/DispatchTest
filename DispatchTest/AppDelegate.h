ed448cdb2c7a18d51debc1ecfc014979f712e05d
joeyharry110 <chenzet@163.com>
2015-04-19 13:16:15 +0800

    DispatchTest
    DispatchTest

---
+++ DispatchTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DispatchTest
+//
+//  Created by chenzetong on 15/4/19.
+//  Copyright (c) 2015年 chenzetong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DispatchTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DispatchTest
+//
+//  Created by chenzetong on 15/4/19.
+//  Copyright (c) 2015年 chenzetong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
